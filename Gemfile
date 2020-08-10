3a3ea804a5d13cb77bc7df9171c955480ae9887c
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2020-08-10 21:07:46 +0900

    create i18n
    install i18n
    install bootstrap
    add not_null_book_title
    create header and footer

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
@@ -24,11 +26,11 @@ gem "jbuilder", "~> 2.7"
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-
+gem "awesome_print"
+gem "rails-i18n"
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", ">= 1.4.2", require: false
 gem "carrierwave"
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,20 @@
+# frozen_string_literal: true
+
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :set_book, only: %i[show edit update destroy]
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   def index
     @books = Book.all
+    @time = Time.now
   end
 
   def show
@@ -17,36 +29,24 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: "Book was successfully created." }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.save
+      redirect_to @book, success: t("directory.flash.create")
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: "Book was successfully updated." }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.update(book_params)
+      redirect_to @book, success: t("directory.flash.update")
+    else
+      render :edit
     end
   end
 
   def destroy
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to books_url, success: t("directory.flash.destroy")
   end
 
   private
