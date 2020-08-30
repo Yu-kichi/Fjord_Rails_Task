426b57199e5acc4c949ec8deee602e808388c0eb
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2020-08-30 20:54:49 +0900

    Fixed to be able to register the email address registered on Github

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i(github)
-  def self.create_unique_string
-     SecureRandom.uuid
-  end
- 
+
   def self.find_for_github_oauth(auth, signed_in_resource=nil)
      user = User.find_by(provider: auth.provider, uid: auth.uid)
  
@@ -16,17 +13,14 @@ class User < ApplicationRecord
        user = User.new(provider: auth.provider,
                        uid:      auth.uid,
                        name:     auth.info.name,
-                       email:    User.dummy_email(auth),
+                       email:    auth.info.email,
                        password: Devise.friendly_token[0, 20]
        )
      end
      user.save
      user
   end
-      
-  def self.dummy_email(auth)
-    "#{auth.uid}-#{auth.provider}@example.com"
-  end
+
   has_many :books, dependent: :destroy
   validates :name, presence: true
   validates :address, length: { maximum: 80 }
