46a67d1b9ed6624aa8a25b5124a1bdffb0cda71f
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2020-09-01 21:23:41 +0900

    Enabled to register with email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,21 +6,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i(github)
 
-  def self.find_for_github_oauth(auth, signed_in_resource=nil)
-     user = User.find_by(provider: auth.provider, uid: auth.uid)
- 
-     unless user
-       user = User.new(provider: auth.provider,
-                       uid:      auth.uid,
-                       name:     auth.info.name,
-                       email:    auth.info.email,
-                       password: Devise.friendly_token[0, 20]
-       )
-     end
-     user.save
-     user
+  validates :uid, presence: true, uniqueness: { scope: :provider }
+  # 通常サインアップ時のuid用、Twitter OAuth認証時のemail用にuuidな文字列を生成
+  def self.create_unique_string
+    SecureRandom.uuid
   end
-
+  
+  def self.find_for_github_oauth(auth, signed_in_resource=nil)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name 
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
   has_many :books, dependent: :destroy
   validates :name, presence: true
   validates :address, length: { maximum: 80 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: "users/registrations",
-    sessions:      "users/sessions",
-    passwords:     "users/passwords",
-    omniauth_callbacks:  "users/omniauth_callbacks" 
-   }
+  devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+  
   scope "(:locale)", locale: /en|ja/ do
-    #root :to => 'oauth_test#index'
     root "books#index"
     resources :books
+    devise_for :users, skip: :omniauth_callbacks, controllers: {
+      registrations: "users/registrations",
+      sessions:      "users/sessions",
+      passwords:     "users/passwords",
+     }
     resources :users, except: [:new, :create] do
       resources :books, only: [:index]
     end
