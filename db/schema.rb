1df28a015746d5a5512841783b738ec736ca1a4a
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2020-09-18 20:51:28 +0900

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_09_22_064129) do
+=======
+ActiveRecord::Schema.define(version: 2020_09_10_124018) do
+
+>>>>>>> e72f10b... change schema
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
